f78898c1834a76d7d5cc7c5d456d0af929147828
Danny Lindquist <dannylindquist@gmail.com>
2019-10-10 21:43:00 -0700

    remove dist

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-declare type RoughTimeObject = {
-    date: Date;
-    hour: string;
-    estimate: string;
-    transition: string;
-    toString: () => string;
-};
-export default function roughTime(date?: Date): RoughTimeObject;
-export {};
